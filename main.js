f8540cd653d7d846b98ac24e0a940c121cbe5421
MohamedReda94 <mohammed.fekri255@gmail.com>
2023-03-23 17:16:40 +0200

    firstJS

---
+++ main.js
@@ -0,0 +1,51 @@
+document.getElementById("average").innerHTML=(1+2+3+4+5+6+7+8+9+10)/10;
+let usection=document.querySelector(".pragraph");
+let isshow = true;
+function show(){
+    if (isshow){
+    usection.style.display="none";
+    isshow=false;
+}else{
+    usection.style.display="block";
+    isshow=true;
+
+}
+}
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+
+  function formvalidate(){
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var password=document.getElementById("password").value;
+    var error=document.getElementById("error");
+    var text="";
+
+    if (name.length<3){
+        text="please entre valid username";
+        error.innerHTML=text;
+        return false;
+    }
+    else if(email.indexOf("@")==-1){
+        text="please entre valid email";
+        error.innerHTML=text;
+        return false;
+    }
+    else if(isNaN(password)){
+        text="please entre valid password";
+        error.innerHTML=text;
+        return false;
+    }
+    else{
+        alert("Done")
+        return true;
+    }
+
+  }
